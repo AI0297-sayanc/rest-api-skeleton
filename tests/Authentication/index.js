80203b23af7c39c42d654333f5a677ce251532dd
Sayan Chakrabarti <sayanc@aggregateintelligence.com>
2022-05-04 18:13:26 +0530

    feat(testing): set up basic testing using Ava & Supertest

---
+++ tests/Authentication/index.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-unresolved
+const test = require("ava")
+const request = require("supertest")
+
+const app = require("../../app")
+
+test("My First Test!", async (t) => {
+  const response = await request(app).post("/auth/token")
+  t.is(response.status, 501)
+  t.assert(["not", "implement"].every((word) => response.text.toLowerCase().includes(word)))
+})
