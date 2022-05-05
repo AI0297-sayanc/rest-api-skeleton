342a350fa9063b990bab76c14b1222186c9c9ba7
Sayan Chakrabarti <sayanc@aggregateintelligence.com>
2022-05-05 19:07:54 +0530

    test(mssql): update test def to include mssql connections

--- tests/Authentication/index.js
+++ tests/Authentication/index.js
@@ -3,6 +3,9 @@ const test = require("ava")
 const request = require("supertest")
 
 const app = require("../../app")
+const mssql = require("../../db/mssql")
+
+test.before(async () => mssql.connect())
 
 test("My First Test!", async (t) => {
   const response = await request(app).post("/auth/token")
